870fce1499e5f4cd9ae28cfe0b4601d58e7a14ae
Looksini David <looksinidavid06@gmail.com>
2025-07-30 06:45:31 +0530

    initial version v1.0

---
+++ Book Library Manager/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Book_Library_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.Author = new System.Windows.Forms.Label();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.lstBooks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(351, 109);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(208, 22);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(351, 169);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(208, 22);
+            this.txtAuthor.TabIndex = 1;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Location = new System.Drawing.Point(231, 109);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(46, 17);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "label1";
+            // 
+            // Author
+            // 
+            this.Author.AutoSize = true;
+            this.Author.Location = new System.Drawing.Point(231, 174);
+            this.Author.Name = "Author";
+            this.Author.Size = new System.Drawing.Size(46, 17);
+            this.Author.TabIndex = 3;
+            this.Author.Text = "label2";
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(420, 232);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(138, 39);
+            this.btnAddBook.TabIndex = 4;
+            this.btnAddBook.Text = "Add Book";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.BtnAddBook_Click);
+            // 
+            // lstBooks
+            // 
+            this.lstBooks.FormattingEnabled = true;
+            this.lstBooks.ItemHeight = 16;
+            this.lstBooks.Location = new System.Drawing.Point(181, 307);
+            this.lstBooks.Name = "lstBooks";
+            this.lstBooks.Size = new System.Drawing.Size(500, 116);
+            this.lstBooks.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstBooks);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.Author);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Author;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.ListBox lstBooks;
+    }
+}
+
---
+++ Book Library Manager/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_Library_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddBook_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string author = txtAuthor.Text;
+            lstBooks.Items.Add(title + " by " + author);
+            txtTitle.Clear();
+            txtAuthor.Clear();
+           
+
+        }
+    }
+}
